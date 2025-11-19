f95f85d3ef9bcb73aa527b4d03b28f752652ec1f
Omkar4042 <omkardalvi7755@gmail.com>
2025-11-19 06:41:30 +0000

    new dev

--- .devcontainer/01_basics/test.js
+++ .devcontainer/01_basics/test.js
@@ -1,2 +1,9 @@
-console.log("Hello, JavaScript in DevContainer!");
-node .devcontainer/01_basics/test.js
+// console.log("Hello, JavaScript in DevContainer!");
+// node .devcontainer/01_basics/test.js
+// to save a file in which we have made changees , 
+// need to go 4 th icon chain type then in it on files
+//  names see and click plus sing when all done then 
+//  in meesage type message and click on commint, 
+//  and after that  at same place after file name
+//   click on three dot and say push to push the changes 
+//   to github repository.

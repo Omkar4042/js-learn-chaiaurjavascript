eaa766a7124ce9a59328fd3b4db7a96950611aa4
Omkar4042 <omkardalvi7755@gmail.com>
2025-11-20 20:07:20 +0000

    maths and nums

---
+++ .devcontainer/01_basics/05_string.js
@@ -0,0 +1,24 @@
+// const name = "omkar"
+// const repocount = 50
+
+// console.log(`hello my name is ${name} and my repcount is ${repocount}.`);
+
+// const gameName = new String ('omkar')
+// // new here is booking an object in backend , check , 
+// // this is another way to declear a string 
+
+// console.log(gameName[0]);
+// // console.log(gameName.__proto__.);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('k'));
+
+// const newString = gameName.substring(0,4)
+
+// console.log(newString)
+
+// const anotherString = gameName.slice(-8,4)
+
+// console.log(anotherString);
--- .devcontainer/01_basics/datatypes-summary.js
+++ .devcontainer/01_basics/datatypes-summary.js
@@ -30,3 +30,28 @@
 
 // const bigNumber  = 233354354545343
 
+// ########### STACK , HEAP ########
+// all premitive DT use STACK
+// all non-premitive use heap
+// in stack when we refer to original element then we are actually to the copy of the original value so if we make any changes in refrence of  original , then the actuall original is not effected.
+
+// Now for heap, when we refer to OG value then actually we are refering to the oG value so all changes made will effect the OG one 
+
+// let myYoutubename = "om"
+// let anothername = myYoutubename
+// anothername = "omkar"
+
+// console.log(myYoutubename);
+// console.log(anothername);
+
+// let userOne = {
+//     email : "user@google.com",
+//     upi:"user@ybl"
+// }
+
+// let userTwo = userOne
+
+// userTwo.email = "omkar@google.com"
+
+// console.log(userOne.email);
+// console.log(userTwo.email);

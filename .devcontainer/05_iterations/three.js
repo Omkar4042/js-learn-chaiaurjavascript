ad691cda4a10216107a82b0c71c0a6b8b1553b67
Omkar4042 <omkardalvi7755@gmail.com>
2025-11-24 12:54:31 +0000

    array specific loops

---
+++ .devcontainer/05_iterations/three.js
@@ -0,0 +1,10 @@
+// Array specific loops 
+
+// for of 
+// here element is i  refrenece variale, and object is the varaiavle diclered on whome we wil iterate
+
+const arr = [1,2,3,4,5,]
+ for (const num of arr) {
+    console.log(num);
+    
+ }

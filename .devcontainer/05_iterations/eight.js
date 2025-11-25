0dba5a7628e94d9308402762a52608fbd47d5801
Omkar4042 <omkardalvi7755@gmail.com>
2025-11-25 12:27:06 +0000

    dom

---
+++ .devcontainer/05_iterations/eight.js
@@ -0,0 +1,29 @@
+// const myNums = [1,2,3]
+
+// // const myTotal = myNums.reduce(function (accumilator,currentvalue){
+// //     console.log(`accumilator ${accumilator} and currentvalue:${currentvalue}`);
+// //     return accumilator + currentvalue
+// // },0)
+
+// // using arrow  function
+// // const myTotal = myNums.reduce((acc,curr)=> acc+curr,0)
+// // console.log(myTotal);
+
+// const shoppingCart = [
+// {
+//     itemsName: "js course",
+//     price:999
+// },
+// {
+//    itemsName: "python course",
+//     price:2999 
+// },
+// {
+//    itemsName: "data science course",
+//     price:5999 
+// }
+// ]
+
+// const priceToPay = shoppingCart.reduce((acc,item)=>acc+item.price,0)
+
+// console.log(priceToPay)
